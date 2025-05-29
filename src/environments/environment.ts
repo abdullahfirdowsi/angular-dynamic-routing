7fedea6d274301ab82918d2223f661a20341a7e4
abdullahfirdowsi <abdullahtrickings@gmail.com>
2025-05-29 15:08:27 +0530

    Update environment: MonsterASP api

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5275/api',
+  // apiUrl: 'http://localhost:5275/api',
+  apiUrl: 'https://backend-dynamic-routing.runasp.net/api',
   jwtTokenKey: 'auth_token'
 };
 
